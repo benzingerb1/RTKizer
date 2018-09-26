d276adb418a056014e21c9888d751a5e62e716ab
benzingerb1 <benzingerb1@gmail.com>
2018-09-25 23:12:50 -0400

    made a complete mess out of the python script

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,6 +1,23 @@
 import csv
+from operator import itemgetter
+
 with open('rtk2.csv', newline='', encoding='utf-8') as csvfile:
      reader = csv.reader(csvfile, delimiter=',')
+     
+     header = next(reader)
+     rows = [header] + [[row[0], row[1], int(row[4])] for row in reader]
+     
+     for row in rows:
+          print(row)
+     
+     
+     
+     csvRtkIndex = 4
+     
+     reader = sorted(reader, key=itemgetter(csvRtkIndex))
      for row in reader:
-          #print(row[0])
-          print(row[1])
+          kanji = row[0]
+          english = row[1]
+          if english == '':
+               continue
+          print(f'"{english}" : "{kanji}",')
